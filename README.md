7c94327b1528ff37ca38ea197b841aa8c03022c0
jzerez <Jonathan.zerez@students.olin.edu>
2021-04-12 18:48:33 -0400

    readme and code comments

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Webots_Playground
- tryig to understand webots
+An introductory WeBots Project with the end goal of creating a 3D simulation of swarm robots. Controller code for this project was written in C++.
+
+## Environment Setup
+**TL;DR: Download [Webots](https://cyberbotics.com/) and make sure you have Python v3+**
+
+The simulation is built off of [Webots](https://cyberbotics.com/), which is an open source robot simulator similar to gazebo. It support various different programming languages such as C, C++, Python, Java, and Matlab, as well as frameworks like ROS.
+
+Additional python scripts have also been created in order to make the process of creating WeBots simulations and worlds a bit more streamlined. Make sure you have some kind of Python version 3.
+
+## File Structure
+
+#### `./controllers` Directory
+The `./controllers` directory contains folders for each of the controller files. **The main controller for this project can be found in [flippy_controller.cpp](./controllers/flippy_controller/flippy_controller.cpp)**
+
+#### `./plugins` Directory
+This contains folders for various custom physics plugin files. **This project does not rely on any custom physics plugins**. If one wanted to create customized physics behaviors, the plugins would live in this directory. For more information about this, checkout [Webot's Documentation on physics plugins.](https://cyberbotics.com/doc/reference/physics-plugin)
+
+#### `./protos` Directory
+This contains `.proto` files. These files are essentially wrappers for complex Webots objects. **This project uses [flippy.proto](./protos/flippy.proto) as a wrapper for the flippy robot object**. These files are used for specifying object parameters and architectures, giving the user a simplified set of parameters to interact with in the Webots UI. It substantially simplifies the feature tree of the project. [More information about protos can be found here](https://cyberbotics.com/doc/guide/tutorial-7-your-first-proto).
+
+## Supplementary Scripts
+None as of 04/12/2021
+
+## More information and documentation
+TODO
+
+## References
+* [Flippy](https://ssr.seas.harvard.edu/files/ssr/files/phdthesis2020malley.pdf)
+*
--- controllers/flippy_controller/flippy_controller.cpp
+++ controllers/flippy_controller/flippy_controller.cpp
@@ -1,30 +1,34 @@
 // File:          flippy_controller.cpp
-// Date:
-// Description:
-// Author:
+// Date:          Spring, 2021
+// Description:   Basic controller for 3D flippy robot
+// Author:        Jonathan Zerez
 // Modifications:
 
-// You may need to add webots include files such as
-// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
-// and/or to add some other includes
+// Include Standar WeBots libraries
 #include <webots/Robot.hpp>
 #include <webots/TouchSensor.hpp>
 #include <webots/Motor.hpp>
 #include <webots/Device.hpp>
 #include <webots/Receiver.hpp>
 #include <webots/Connector.hpp>
+
+// Include standard C++ libraries
 #include <string>
 #include <iostream>
 #include <cstring>
+
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
 
+const float VELOCITY = 1.0;
 bool pivot(
   TouchSensor* curr_moving_sensor,
   Motor* curr_moving_motor,
   Motor* curr_stationary_motor,
   Connector* curr_moving_connector,
   Connector* curr_stationary_connector,
+  Connector* curr_moving_sym_connector,
+  Connector* curr_stationary_sym_connector,
   std::string debug = "None"
 ){
   // checks to see if the moving node has made contact, and then switches the moving node
@@ -32,33 +36,34 @@ bool pivot(
   if (curr_moving_sensor->getValue() == 1) {
     std::cout << debug << std::endl;
     curr_moving_motor->setVelocity(0.0);
-    if (curr_moving_connector->getPresence() == 1) {
+    if (curr_moving_sym_connector->getPresence() == 1){
       curr_stationary_connector->unlock();
+      curr_stationary_sym_connector->unlock();
+      curr_moving_sym_connector->lock();
+      curr_stationary_motor->setVelocity(VELOCITY);
+      std::cout << "HIT OTHER FLIPPY" << std::endl;
+      return true;
+    } else if (curr_moving_connector->getPresence() == 1) {
+      curr_stationary_connector->unlock();
+      curr_stationary_sym_connector->unlock();
       curr_moving_connector->lock();
-      curr_stationary_motor->setVelocity(1.0);
+      curr_stationary_motor->setVelocity(VELOCITY);
+      std::cout << "HIT FLOOR" << std::endl;
       return true;
     } else {
       std::cout << "NO JOINT FOUND" << std::endl;
     }
   }
   return false;
-  
-}
 
-void print(std::string out, bool newLine=true) {
-  if (newLine) {
-    std::cout << out << std::endl;
-  } else {
-    std::cout << out;
-  }
 }
-
 
 // This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
+// It creates an instance of the Robot instance, launches its
 // function(s) and destroys it at the end of the execution.
 // Note that only one instance of Robot should be created in
 // a controller program.
+
 // The arguments of the main function can be specified by the
 // "controllerArgs" field of the Robot node
 int main(int argc, const char *argv[]) {
@@ -67,23 +72,22 @@ int main(int argc, const char *argv[]) {
   // create the Robot instance.
   Robot* robot = new Robot();
 
-  // get the time step of the current world.
+  // get the time step of the current world (in milliseconds)
   int timeStep = (int)robot->getBasicTimeStep();
 
   // Initialize motors (pointers)
   Motor* m1 = robot->getMotor("M1");
   Motor* m2 = robot->getMotor("M2");
 
+  // Initialize motor operating modes (INFINITY means velocity driven)
   m1->setPosition(INFINITY);
   m1->setVelocity(0.0);
   m2->setPosition(INFINITY);
-  m2->setVelocity(1.0);
+  m2->setVelocity(VELOCITY);
 
   // Initialize touchsensors (pointers)
   TouchSensor* t1 = robot->getTouchSensor("T1");
   TouchSensor* t2 = robot->getTouchSensor("T2");
-
-
 
   // Enable touch sensors. Sets refresh frequency to timeStep
   t1->enable(timeStep);
@@ -94,33 +98,41 @@ int main(int argc, const char *argv[]) {
   // TODO: see if a symetric connector will work without being defined in cpp
   Connector* s1_joint = robot->getConnector("s1_connector");
   Connector* s2_joint = robot->getConnector("s2_connector");
+  Connector* s1_sym_joint = robot->getConnector("s1_sym_connector");
+  Connector* s2_sym_joint = robot->getConnector("s2_sym_connector");
 
   // Set the query frequency in ms
-  s1_joint->enablePresence(10);
-  s2_joint->enablePresence(10);
+  s1_joint->enablePresence(timeStep);
+  s2_joint->enablePresence(timeStep);
+  s1_sym_joint->enablePresence(timeStep);
+  s2_sym_joint->enablePresence(timeStep);
 
-  // Lock the connections in place
+  // Lock the connections in place (no relative movement)
   // s1_joint->lock();
   s2_joint->lock();
 
-  // which sphere is moving. 0 for none, 1 for sphere1, 2 for sphere2
-  // int moving_sphere = std::stoi(argv[0]);
+  // Which sphere is moving. 0 for none, 1 for sphere1, 2 for sphere2
   int moving_sphere;
-
-  
-  std::cout << argv[1] << std::endl;
+=
+  // The first argument in controllerArgs specifies which sphere is moving
   if (strcmp(argv[1], "0") == 0) {
     moving_sphere = 0;
-    std::cout << "STATE IS: 0" << std::endl;
+    m1->setPosition(INFINITY);
+    m1->setVelocity(0.0);
+    m2->setPosition(INFINITY);
+    m2->setVelocity(0.0);
+    s1_joint->lock();
+    s2_joint->lock();
+    std::cout << "STATE IS: 0. NO MOVEMENT" << std::endl;
   } else if (strcmp(argv[1], "1") == 0) {
     moving_sphere = 1;
-    std::cout << "STATE IS: 1" << std::endl;
+    std::cout << "STATE IS: 1. SPHERE 1 WILL MOVE FIRST" << std::endl;
   } else if (strcmp(argv[1], "2") == 0) {
     moving_sphere = 2;
-    std::cout << "STATE IS: 2" << std::endl;
+    std::cout << "STATE IS: 2. SPHERE 2 WILL MOVE FIRST" << std::endl;
   } else {
     moving_sphere = 1;
-    std::cout << "STATE IS: DEFAULT 1" << std::endl;
+    std::cout << "STATE IS: DEFAULT. SPHERE 1 WILL MOVE FIRST" << std::endl;
   }
 
 
@@ -130,25 +142,24 @@ int main(int argc, const char *argv[]) {
     switch(moving_sphere) {
       case 0:
         // robot is not moving
-        std::cout << "stationary" << std::endl;
+        // std::cout << "stationary" << std::endl;
         break;
       case 1:
         // Sphere1 is moving about sphere2
-        if (pivot(t1, m2, m1, s1_joint, s2_joint, "SPHERE1 MOVING")) {
+
+        // IF the robot registers a touch through the touch sensor, pivot
+        if (pivot(t1, m2, m1, s1_joint, s2_joint, s1_sym_joint, s2_sym_joint, "SPHERE1 MOVING")) {
           moving_sphere = 2;
         }
         break;
       case 2:
         // sphere2 is moving about sphere1
-        if (pivot(t2, m1, m2, s2_joint, s1_joint, "SPHERE2 MOVING")) {
+        // If the robot registers a touch through the touch sensor, pivot
+        if (pivot(t2, m1, m2, s2_joint, s1_joint,  s2_sym_joint, s1_sym_joint, "SPHERE2 MOVING")) {
           moving_sphere = 1;
         }
         break;
     }
-
-
-
-
     // read the value of the touch sensor at the second sphere
     // double val = t2->getValue();
     // std::cout << val << std::endl;
@@ -156,7 +167,6 @@ int main(int argc, const char *argv[]) {
     // sample code for recieving messages from the physics plug-in (not working)
     // std::cout<< "new message recieved" << std::endl;
     // std::cout<<wb_receiver_get_data(r1)<<std::endl;
-
 
   };
 
