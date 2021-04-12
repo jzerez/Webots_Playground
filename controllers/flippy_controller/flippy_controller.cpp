c7f9d193405199ae018e60d8b6f7a2475a0134f7
jzerez <Jonathan.zerez@students.olin.edu>
2021-04-12 11:43:54 -0400

    controller args and symmetric connectors

--- controllers/flippy_controller/flippy_controller.cpp
+++ controllers/flippy_controller/flippy_controller.cpp
@@ -15,28 +15,42 @@
 #include <webots/Connector.hpp>
 #include <string>
 #include <iostream>
-
+#include <cstring>
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
 
-void pivot(
+bool pivot(
   TouchSensor* curr_moving_sensor,
   Motor* curr_moving_motor,
   Motor* curr_stationary_motor,
   Connector* curr_moving_connector,
-  Connector* curr_stationary_connector
+  Connector* curr_stationary_connector,
+  std::string debug = "None"
 ){
   // checks to see if the moving node has made contact, and then switches the moving node
   // NOTE: curr_moving_motor is the one that is spinning. It is NOT translating in space.
   if (curr_moving_sensor->getValue() == 1) {
+    std::cout << debug << std::endl;
     curr_moving_motor->setVelocity(0.0);
     if (curr_moving_connector->getPresence() == 1) {
-      curr_moving_connector->lock();
       curr_stationary_connector->unlock();
+      curr_moving_connector->lock();
       curr_stationary_motor->setVelocity(1.0);
+      return true;
+    } else {
+      std::cout << "NO JOINT FOUND" << std::endl;
     }
   }
+  return false;
   
+}
+
+void print(std::string out, bool newLine=true) {
+  if (newLine) {
+    std::cout << out << std::endl;
+  } else {
+    std::cout << out;
+  }
 }
 
 
@@ -47,7 +61,7 @@ void pivot(
 // a controller program.
 // The arguments of the main function can be specified by the
 // "controllerArgs" field of the Robot node
-int main(int argc, char **argv) {
+int main(int argc, const char *argv[]) {
 
   std::cout << "INITIALIZING CONTROLLER" << std::endl;
   // create the Robot instance.
@@ -90,28 +104,47 @@ int main(int argc, char **argv) {
   s2_joint->lock();
 
   // which sphere is moving. 0 for none, 1 for sphere1, 2 for sphere2
-  int moving_sphere = 1;
+  // int moving_sphere = std::stoi(argv[0]);
+  int moving_sphere;
+
+  
+  std::cout << argv[1] << std::endl;
+  if (strcmp(argv[1], "0") == 0) {
+    moving_sphere = 0;
+    std::cout << "STATE IS: 0" << std::endl;
+  } else if (strcmp(argv[1], "1") == 0) {
+    moving_sphere = 1;
+    std::cout << "STATE IS: 1" << std::endl;
+  } else if (strcmp(argv[1], "2") == 0) {
+    moving_sphere = 2;
+    std::cout << "STATE IS: 2" << std::endl;
+  } else {
+    moving_sphere = 1;
+    std::cout << "STATE IS: DEFAULT 1" << std::endl;
+  }
 
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(timeStep) != -1) {
-      switch(moving_sphere) {
-        case 0:
-          // robot is not moving
-          std::cout << "stationary" << std::endl;
-          break;
-        case 1:
-          // Sphere1 is moving about sphere2
-          pivot(t1, m2, m1, s1_joint, s2_joint);
+    switch(moving_sphere) {
+      case 0:
+        // robot is not moving
+        std::cout << "stationary" << std::endl;
+        break;
+      case 1:
+        // Sphere1 is moving about sphere2
+        if (pivot(t1, m2, m1, s1_joint, s2_joint, "SPHERE1 MOVING")) {
           moving_sphere = 2;
-          break;
-        case 2:
-          // sphere2 is moving about sphere1
-          pivot(t2, m1, m2, s2_joint, s1_joint);
+        }
+        break;
+      case 2:
+        // sphere2 is moving about sphere1
+        if (pivot(t2, m1, m2, s2_joint, s1_joint, "SPHERE2 MOVING")) {
           moving_sphere = 1;
-          break;
-      }
+        }
+        break;
+    }
 
 
 
